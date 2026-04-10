2fc87d58df946b55bf7c80fe1b8ce1acb2b4addc
AAgarwal04 <aryanbansal@utexas.edu>
2026-04-10 01:30:35 -0500

    Augmented Sim + Tests

---
+++ test0.asm
@@ -0,0 +1,6 @@
+ADDI x1, x1, 160
+SLLI x1, x1, 8 #1000
+ADDI x2, x1, 0 #1000
+SLLI x1, x3, 1 #2000
+IPP x1, x3
+
---
+++ test_bias_add.asm
@@ -0,0 +1,8 @@
+ADDI x1, x1, 160
+SLLI x1, x1, 8 #1000
+ADDI x2, x1, 0 #1000
+SLLI x1, x3, 1 #2000
+
+SLLI x4, x3, 1  #4000
+ADD x5, x1, x4 #5000
+BADD x3, x4, x5
